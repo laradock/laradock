686791af6c5a45c5b42b5ac189dabf868b0353ac
bagart <baltaev.artur@gmail.com>
2017-01-31 19:08:32 +0300

    https://github.com/laradock/laradock/issues/563 \r

--- workspace/aliases.sh
+++ workspace/aliases.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
 # Colors used for status updates
-ESC_SEQ="\x1b["
-COL_RESET=$ESC_SEQ"39;49;00m"
+# ESC_SEQ="\x1b["
+# COL_RESET=$ESC_SEQ"39;49;00m"
 COL_RED=$ESC_SEQ"31;01m"
 COL_GREEN=$ESC_SEQ"32;01m"
 COL_YELLOW=$ESC_SEQ"33;01m"
