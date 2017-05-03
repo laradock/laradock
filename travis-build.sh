bedb663a8e95647bef6c38fe3629adffb05feda9
Shao Yu Lung <bestlong168@gmail.com>
2017-05-03 18:56:01 +0800

    use CI build config set to update PHP_VERSION in .env

--- travis-build.sh
+++ travis-build.sh
@@ -6,6 +6,7 @@ set -e
 #### Build the Docker Images
 if [ -n "${PHP_VERSION}" ]; then
     cp env-example .env
+    sed -i -- "s/PHP_VERSION=.*/PHP_VERSION=${PHP_VERSION}/g" .env
     sed -i -- 's/=false/=true/g' .env
     cat .env
     docker-compose build
