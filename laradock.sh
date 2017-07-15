3e5d1d919a5345ea2c8fe8bfa16533ac1933becb
Arian Acosta <arianmaps@yahoo.com>
2017-07-15 09:56:03 -0400

    running in daemon mode

--- laradock.sh
+++ laradock.sh
@@ -8,7 +8,7 @@ if [ "$1" == "up" ] ; then
     echo "Initializing Docker Sync";
     docker-sync start;
     echo "Initializing Docker Compose";
-    docker-compose -f docker-compose.yml -f docker-compose.sync.yml $@;
+    docker-compose -f docker-compose.yml -f docker-compose.sync.yml $@ -d;
 elif [ "$1" == "down" ]; then
     echo "Stopping Docker Compose";
     docker-compose down;
