22dcf86c96c9d0224de69f098bb37801f4e92021
Schepotins <schepotin@dias.team>
2018-05-12 19:21:00 +0300

    feature(nginx): autogenerate certificates

---
+++ nginx/startup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [ ! -f /etc/nginx/ssl/default.crt ]; then
+    openssl genrsa -out "/etc/nginx/ssl/default.key" 2048
+    openssl req -new -key "/etc/nginx/ssl/default.key" -out "/etc/nginx/ssl/default.csr" -subj "/CN=default/O=default/C=UK"
+    openssl x509 -req -days 365 -in "/etc/nginx/ssl/default.csr" -signkey "/etc/nginx/ssl/default.key" -out "/etc/nginx/ssl/default.crt"
+fi
+
+nginx
