2e2d5dc031fc645953c8cb38b9943eb6b22539f1
marknauf <mnaufel@asu.edu>
2018-07-24 07:55:55 -0700

    Add passport install

--- up.sh
+++ up.sh
@@ -3,4 +3,5 @@
 docker-compose up -d mongo nginx
 
 docker-compose exec workspace bash -c "php artisan migrate";
+docker-compose exec workspace bash -c "php artisan passport:install --force";
 docker exec -it laradock_nginx_1 bash -c "cd /var/www/axio-frontend && ./bin/build.sh";
