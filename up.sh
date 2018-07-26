9eb09675734591443d0d5d57eb85944a1e58b795
marknauf <mnaufel@asu.edu>
2018-07-25 18:05:59 -0700

    Finally working properly

--- up.sh
+++ up.sh
@@ -2,7 +2,7 @@
 
 docker-compose up -d nginx mongo
 
-docker-compose exec workspace bash -c "php artisan cache:clear &&
+docker-compose exec workspace bash -c "cd /var/www/axio-api/ && php artisan cache:clear &&
 chmod -R 777 storage/ && composer dump-autoload && php artisan view:clear &&
 php artisan config:cache && php artisan migrate && php artisan passport:install --force";
 docker-compose exec --user=laradock workspace bash --login -c "cd /var/www/axio-frontend && ./bin/build.sh";
