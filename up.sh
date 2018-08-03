7fb36f2d1bec5ff67057ea801076a06d3e934c76
Chase Adams <chaseadams123@gmail.com>
2018-08-02 20:27:43 -0700

    chore(echo): added echo message for developers after stack is upped

--- up.sh
+++ up.sh
@@ -6,3 +6,5 @@ docker-compose exec workspace bash -c "cd /var/www/axio-api/ && composer install
 chmod -R 777 storage/ && composer dump-autoload && php artisan view:clear &&
 php artisan config:cache && php artisan migrate && php artisan passport:install --force";
 docker-compose exec --user=laradock workspace bash --login -c "cd /var/www/axio-frontend && ./bin/build.sh";
+
+echo "Axio is now available at http://localhost!"
