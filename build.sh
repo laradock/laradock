2ba2df0b9bb00126ddeea4e21af2d959e11cd80a
John <John-lee@2001594.no-reply.drupal.org>
2018-10-31 14:44:18 +0800

    Fix.

--- build.sh
+++ build.sh
@@ -4,19 +4,19 @@
 #                   Build for drupal               #
 ####################################################
 
-docker-compose build workspace nginx mariadb
+docker-compose build workspace nginx mariadb php-fpm
 
 docker rm -f laradock_workspace_1
 docker run -d --name laradock_workspace_1 laradock_workspace
-if [ -d ./drupal.old ]; then
-  sudo rm -rf ./drupal.old
+if [ -d ../drupal.old ]; then
+  sudo rm -rf ../drupal.old
 fi
-if [ -d ./drupal ]; then
-  mv ./drupal ./drupal.old
+if [ -d ../drupal ]; then
+  mv ../drupal ../drupal.old
 fi
-sudo docker cp laradock_workspace_1:/var/www/drupal drupal
+sudo docker cp laradock_workspace_1:/var/www/drupal ../drupal
 docker stop laradock_workspace_1
 docker rm laradock_workspace_1
 
-sudo chown -R $USER ./drupal
-sudo chmod -R g+w ./drupal
+sudo chown -R $USER ../drupal
+sudo chmod -R g+w ../drupal
