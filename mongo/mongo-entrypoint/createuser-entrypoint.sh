b434f6b6041326d2ba95a2dd90a594895d3ac8c5
StefanT123 <stefan8tanevski@gmail.com>
2020-02-25 09:59:54 +0100

    Upgraded laradock

---
+++ mongo/mongo-entrypoint/createuser-entrypoint.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+echo 'Creating application user and db'
+
+mongo ${APP_MONGO_DB} \
+        --host localhost \
+        --port ${MONGODB_PORT} \
+        -u ${MONGO_ROOT_USER} \
+        -p ${MONGO_ROOT_PASS} \
+        --authenticationDatabase admin \
+        --eval "db.adminCommand({ setFeatureCompatibilityVersion: 4.0 }); db.createUser({user: '${APP_MONGO_USER}', pwd: '${APP_MONGO_PASS}', roles:[{role:'dbOwner', db: '${APP_MONGO_DB}'}]});"
