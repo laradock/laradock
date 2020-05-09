3a3f1290c378d3a7cd04f57f138124023fd12940
Noud de Brouwer <noud4@home.nl>
2020-05-09 21:11:32 +0200

    localhost config

---
+++ bin/docker_start.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env sh
+#cd laradock
+docker-compose up -d apache2 php-fpm phpmyadmin mysql mailhog workspace redis
---
+++ bin/docker_workspace.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env sh
+#cd laradock
+docker-compose exec --user=laradock workspace bash
