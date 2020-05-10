c6e92a18f8840f45efce83f307dc9ad6793103f7
Noud de Brouwer <noud4@home.nl>
2020-05-10 17:24:36 +0200

    elasticsearch

--- bin/docker_start.sh
+++ bin/docker_start.sh
@@ -1,3 +1,14 @@
 #!/usr/bin/env sh
 #cd laradock
-docker-compose up -d apache2 php-fpm phpmyadmin mysql mailhog workspace redis
+docker-compose up -d apache2 php-fpm workspace
+
+docker-compose up -d mailhog
+
+docker-compose up -d mysql phpmyadmin
+
+docker-compose up -d redis redis-webui
+
+docker-compose up -d elasticsearch
+docker run -p 1358:1358 -d appbaseio/dejavu
+
+docker-compose up -d docker-web-ui
