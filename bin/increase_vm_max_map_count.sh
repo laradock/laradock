2f3f56e36b05c0b4edd79ac59d79000abdbd30df
Noud de Brouwer <noud4@home.nl>
2020-09-19 23:28:39 +0200

    bin sh

--- bin/increase_vm_max_map_count.sh
+++ bin/increase_vm_max_map_count.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env sh
+
 # dev mode gives
 # elasticsearch_1        | [1]: max virtual memory areas vm.max_map_count [65530] is too low, increase to at least [262144]
 sudo sysctl -w vm.max_map_count=262144
