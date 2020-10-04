50dab10ff5da6b78396faa7a5e32d359314a54bb
Kim Francis Corros <corros121@gmail.com>
2020-10-04 14:28:42 +0800

    -// Exclude createdb.sql from gitignore

---
+++ mysql/docker-entrypoint-initdb.d/createdb.sql
@@ -0,0 +1,25 @@
+#
+# Copy createdb.sql.example to createdb.sql
+# then uncomment then set database name and username to create you need databases
+#
+# example: .env MYSQL_USER=appuser and needed db name is myshop_db
+#
+#    CREATE DATABASE IF NOT EXISTS `myshop_db` ;
+#    GRANT ALL ON `myshop_db`.* TO 'appuser'@'%' ;
+#
+#
+# this sql script will auto run when the mysql container starts and the $DATA_PATH_HOST/mysql not found.
+#
+# if your $DATA_PATH_HOST/mysql exists and you do not want to delete it, you can run by manual execution:
+#
+#     docker-compose exec mysql bash
+#     mysql -u root -p < /docker-entrypoint-initdb.d/createdb.sql
+#
+
+CREATE DATABASE IF NOT EXISTS `qsystem_db` COLLATE 'utf8_general_ci' ;
+GRANT ALL ON `qsystem_db`.* TO 'default'@'%' ;
+
+CREATE DATABASE IF NOT EXISTS `qsystem_test` COLLATE 'utf8_general_ci' ;
+GRANT ALL ON `qsystem_test`.* TO 'default'@'%' ;
+
+FLUSH PRIVILEGES ;
