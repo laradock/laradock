3f5bfd1ae3262271ce3ca5a4f182765fd2aeb538
Noud de Brouwer <noud4@home.nl>
2020-12-07 23:22:19 +0100

    scripts

--- sbin/docker_start.sh
+++ sbin/docker_start.sh
@@ -4,32 +4,39 @@ sudo systemctl stop apache2
 sudo systemctl stop mongodb
 
 # Docker
-sudo usermod -a -G docker noud
+sudo usermod -aG docker $USER
 sudo systemctl restart docker
 
 # memmory for elasticsearch
 sudo sysctl -w vm.max_map_count=262144
 
-# export BUILD="--build"
-export BUILD=""
+export BUILD="" # '--build'
+export WORKSPACE_INSTALL_KRB5=false
 
-declare -a arr=("apache2" "php-fpm" "workspace" \
-# infra \
-"kibana" "dejavu" "manifoldcf" \
+declare -a arr=("apache2" \
+"docker-registry" "docker-web-ui" \
+"dejavu" "kibana" "manifoldcf" \
 "memcached" "redis-webui" \
-# It looks like you are trying to access MongoDB over HTTP on the native driver port. \
-"mongo-webui" \
-"mysql" "phpmyadmin" \
-# PermissionError: [Errno 1] Operation not permitted: '/var/lib/pgadmin/sessions' \
-"postgres" "pgadmin" \
-"rabbitmq" \
-"sqs" \
-# docker \
-"docker-web-ui" \
-# mail \
-# "mailcatcher" \
-"mailhog" \
+"mongo" \
+"phpmyadmin" \
+"pgadmin" \
+"rabbitmq" "sqs" \
+"mailhog"
 )
+
+# above also includes
+# "php-fpm" "workspace"
+# "docker"
+# "redis"
+# "mysql"
+# "postgres"
+
+# above can optinally do
+# "mailcatcher"
+
+# above fails for
+# "mongo-webui" # It looks like you are trying to access MongoDB over HTTP on the native driver port.
+# PermissionError: [Errno 1] Operation not permitted: '/var/lib/pgadmin/sessions'
 
 for i in "${arr[@]}"
 do
---
+++ sbin/install.sh
@@ -0,0 +1,30 @@
+# sudo apt-get remove -y docker docker-engine docker.io containerd runc
+# sudo apt remove --purge -y python-pip python3-pip
+# sudo apt remove --purge -y python3
+# sudo apt autoremove
+
+sudo apt-get update
+sudo apt-get install -y \
+   apt-transport-https \
+   ca-certificates \
+   curl \
+   gnupg-agent \
+   software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+# --allow-insecure-repositories
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+
+apt-cache madison docker-ce
+sudo apt-get install -y docker-ce=5:19.03.14~3-0~ubuntu-focal docker-ce-cli=5:19.03.14~3-0~ubuntu-focal containerd.io
+sudo docker run hello-world
+sudo usermod -aG docker $USER
+
+sudo apt install -y python3-distutils
+sudo apt install -y python3-pip
+sudo pip install pyyaml
