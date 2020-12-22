fbda1f542f2f357f7c7daf643cf7d499182354ac
Ilya Apasov <apasov@users.noreply.github.com>
2020-12-22 16:27:31 +0200

    fix incomplete SSL chain error
    fixes #2804

--- certbot/run-certbot.sh
+++ certbot/run-certbot.sh
@@ -3,5 +3,7 @@
 letsencrypt certonly --webroot -w /var/www/letsencrypt -d "$CN" --agree-tos --email "$EMAIL" --non-interactive --text
 
 cp /etc/letsencrypt/archive/"$CN"/cert1.pem /var/certs/"$CN"-cert1.pem
+cp /etc/letsencrypt/archive/"$CN"/chain1.pem /var/certs/chain1.pem
+cp /etc/letsencrypt/archive/"$CN"/fullchain1.pem /var/certs/fullchain1.pem
 cp /etc/letsencrypt/archive/"$CN"/privkey1.pem /var/certs/"$CN"-privkey1.pem
 
