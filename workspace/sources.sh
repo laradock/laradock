7e8fe6db689af5726cccc080727267e61534d3ff
zhushaolong <15210900722@163.com>
2021-02-26 11:52:04 +0800

    Update sources.sh
    update source list

--- workspace/sources.sh
+++ workspace/sources.sh
@@ -9,71 +9,67 @@ if type "tee" 2>/dev/null && [ -n "${UBUNTU_SOURCE}" ]; then
         "aliyun")
             tee ${SOURCE_PATH} <<-'EOF'
 deb http://mirrors.aliyun.com/ubuntu/ bionic main restricted universe multiverse
-deb http://mirrors.aliyun.com/ubuntu/ bionic-security main restricted universe multiverse
-deb http://mirrors.aliyun.com/ubuntu/ bionic-updates main restricted universe multiverse
-deb http://mirrors.aliyun.com/ubuntu/ bionic-proposed main restricted universe multiverse
-deb http://mirrors.aliyun.com/ubuntu/ bionic-backports main restricted universe multiverse
 deb-src http://mirrors.aliyun.com/ubuntu/ bionic main restricted universe multiverse
+
+deb http://mirrors.aliyun.com/ubuntu/ bionic-security main restricted universe multiverse
 deb-src http://mirrors.aliyun.com/ubuntu/ bionic-security main restricted universe multiverse
+
+deb http://mirrors.aliyun.com/ubuntu/ bionic-updates main restricted universe multiverse
 deb-src http://mirrors.aliyun.com/ubuntu/ bionic-updates main restricted universe multiverse
+
+deb http://mirrors.aliyun.com/ubuntu/ bionic-proposed main restricted universe multiverse
 deb-src http://mirrors.aliyun.com/ubuntu/ bionic-proposed main restricted universe multiverse
+
+deb http://mirrors.aliyun.com/ubuntu/ bionic-backports main restricted universe multiverse
 deb-src http://mirrors.aliyun.com/ubuntu/ bionic-backports main restricted universe multiverse
 EOF
 ;;
         "zju")
             tee ${SOURCE_PATH} <<-'EOF'
-deb http://mirrors.zju.edu.cn/ubuntu/ bionic main multiverse restricted universe
-deb http://mirrors.zju.edu.cn/ubuntu/ bionic-backports main multiverse restricted universe
-deb http://mirrors.zju.edu.cn/ubuntu/ bionic-proposed main multiverse restricted universe
-deb http://mirrors.zju.edu.cn/ubuntu/ bionic-security main multiverse restricted universe
-deb http://mirrors.zju.edu.cn/ubuntu/ bionic-updates main multiverse restricted universe
-deb-src http://mirrors.zju.edu.cn/ubuntu/ bionic main multiverse restricted universe
-deb-src http://mirrors.zju.edu.cn/ubuntu/ bionic-backports main multiverse restricted universe
-deb-src http://mirrors.zju.edu.cn/ubuntu/ bionic-proposed main multiverse restricted universe
-deb-src http://mirrors.zju.edu.cn/ubuntu/ bionic-security main multiverse restricted universe
-deb-src http://mirrors.zju.edu.cn/ubuntu/ bionic-updates main multiverse restricted universe
+deb http://mirrors.zju.edu.cn/ubuntu bionic main universe restricted multiverse
+deb http://mirrors.zju.edu.cn/ubuntu bionic-security main universe restricted multiverse
+deb http://mirrors.zju.edu.cn/ubuntu bionic-updates main universe restricted multiverse
+deb http://mirrors.zju.edu.cn/ubuntu bionic-backports main universe restricted multiverse
+deb-src http://mirrors.zju.edu.cn/ubuntu bionic main universe restricted multiverse
+deb-src http://mirrors.zju.edu.cn/ubuntu bionic-security main universe restricted multiverse
+deb-src http://mirrors.zju.edu.cn/ubuntu bionic-updates main universe restricted multiverse
+deb-src http://mirrors.zju.edu.cn/ubuntu bionic-backports main universe restricted multiverse
 EOF
 ;;
         "tsinghua")
             tee ${SOURCE_PATH} <<-'EOF'
-deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic main restricted universe multiverse
-deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic main restricted universe multiverse
-deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-updates main restricted universe multiverse
-deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-updates main restricted universe multiverse
-deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-backports main restricted universe multiverse
-deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-backports main restricted universe multiverse
-deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-security main restricted universe multiverse
-deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-security main restricted universe multiverse
-deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-proposed main restricted universe multiverse
-deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-proposed main restricted universe multiverse
+deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic main universe restricted multiverse
+deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-security main universe restricted multiverse
+deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-updates main universe restricted multiverse
+deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-backports main universe restricted multiverse
+deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic main universe restricted multiverse
+deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-security main universe restricted multiverse
+deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-updates main universe restricted multiverse
+deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-backports main universe restricted multiverse
 EOF
 ;;
         "163")
             tee ${SOURCE_PATH} <<-'EOF'
-deb http://mirrors.163.com/ubuntu/ bionic main restricted universe multiverse
-deb http://mirrors.163.com/ubuntu/ bionic-security main restricted universe multiverse
-deb http://mirrors.163.com/ubuntu/ bionic-updates main restricted universe multiverse
-deb http://mirrors.163.com/ubuntu/ bionic-proposed main restricted universe multiverse
-deb http://mirrors.163.com/ubuntu/ bionic-backports main restricted universe multiverse
-deb-src http://mirrors.163.com/ubuntu/ bionic main restricted universe multiverse
-deb-src http://mirrors.163.com/ubuntu/ bionic-security main restricted universe multiverse
-deb-src http://mirrors.163.com/ubuntu/ bionic-updates main restricted universe multiverse
-deb-src http://mirrors.163.com/ubuntu/ bionic-proposed main restricted universe multiverse
-deb-src http://mirrors.163.com/ubuntu/ bionic-backports main restricted universe multiverse
+deb http://mirrors.163.com/ubuntu/ bionic main universe restricted multiverse
+deb http://mirrors.163.com/ubuntu/ bionic-security main universe restricted multiverse
+deb http://mirrors.163.com/ubuntu/ bionic-updates main universe restricted multiverse
+deb http://mirrors.163.com/ubuntu/ bionic-backports main universe restricted multiverse
+deb-src http://mirrors.163.com/ubuntu/ bionic main universe restricted multiverse
+deb-src http://mirrors.163.com/ubuntu/ bionic-security main universe restricted multiverse
+deb-src http://mirrors.163.com/ubuntu/ bionic-updates main universe restricted multiverse
+deb-src http://mirrors.163.com/ubuntu/ bionic-backports main universe restricted multiverse
 EOF
 ;;
         "ustc")
             tee ${SOURCE_PATH} <<-'EOF'
-deb https://mirrors.ustc.edu.cn/ubuntu/ bionic main restricted universe multiverse
-deb-src https://mirrors.ustc.edu.cn/ubuntu/ bionic main restricted universe multiverse
-deb https://mirrors.ustc.edu.cn/ubuntu/ bionic-updates main restricted universe multiverse
-deb-src https://mirrors.ustc.edu.cn/ubuntu/ bionic-updates main restricted universe multiverse
-deb https://mirrors.ustc.edu.cn/ubuntu/ bionic-backports main restricted universe multiverse
-deb-src https://mirrors.ustc.edu.cn/ubuntu/ bionic-backports main restricted universe multiverse
-deb https://mirrors.ustc.edu.cn/ubuntu/ bionic-security main restricted universe multiverse
-deb-src https://mirrors.ustc.edu.cn/ubuntu/ bionic-security main restricted universe multiverse
-deb https://mirrors.ustc.edu.cn/ubuntu/ bionic-proposed main restricted universe multiverse
-deb-src https://mirrors.ustc.edu.cn/ubuntu/ bionic-proposed main restricted universe multiverse
+deb https://mirrors.ustc.edu.cn/ubuntu/ bionic main universe restricted multiverse
+deb https://mirrors.ustc.edu.cn/ubuntu/ bionic-security main universe restricted multiverse
+deb https://mirrors.ustc.edu.cn/ubuntu/ bionic-updates main universe restricted multiverse
+deb https://mirrors.ustc.edu.cn/ubuntu/ bionic-backports main universe restricted multiverse
+deb-src https://mirrors.ustc.edu.cn/ubuntu/ bionic main universe restricted multiverse
+deb-src https://mirrors.ustc.edu.cn/ubuntu/ bionic-security main universe restricted multiverse
+deb-src https://mirrors.ustc.edu.cn/ubuntu/ bionic-updates main universe restricted multiverse
+deb-src https://mirrors.ustc.edu.cn/ubuntu/ bionic-backports main universe restricted multiverse
 EOF
 ;;
         *)
