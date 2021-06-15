a76ec9c3f987adca3c23c8b4b676b06bb73cf0ca
Nyongesa Ignatius <ignatius.freelancer@gmail.com>
2021-06-15 19:17:42 +0300

    added multi-domain certbot capabilities with dry-runs feature

--- certbot/run-certbot.sh
+++ certbot/run-certbot.sh
@@ -1,9 +1,58 @@
 #!/bin/bash
 
-letsencrypt certonly --webroot -w /var/www/letsencrypt -d "$CN" --agree-tos --email "$EMAIL" --non-interactive --text
+#-----------------------------------------------------------------------
+#STEP 1) make vars 
 
-cp /etc/letsencrypt/archive/"$CN"/cert1.pem /var/certs/"$CN"-cert1.pem
-cp /etc/letsencrypt/archive/"$CN"/chain1.pem /var/certs/chain1.pem
-cp /etc/letsencrypt/archive/"$CN"/fullchain1.pem /var/certs/fullchain1.pem
-cp /etc/letsencrypt/archive/"$CN"/privkey1.pem /var/certs/"$CN"-privkey1.pem
+#clean environment variables (stripping off redundant quotes if any)
+environment="${ENV%\"}"
+environment="${environment#\"}"
+
+#clean email address (stripping off redundant quotes if any)
+email="${EMAIL%\"}"
+email="${email#\"}"
+
+#clean domain names (stripping off redundant quotes if any)
+domains="${CN%\"}"
+domains="${domains#\"}"
+
+
+CONTACT="$email"
+DOMAINS="$domains"
+
+echo "EMAIL: $CONTACT"
+echo "DOMAINS: $DOMAINS"
+#-----------------------------------------------------------------------
+
+#-----------------------------------------------------------------------
+#STEP 2) get ssl certificates
+printf "MAKING SSL CERTIFICATE FOR \n"
+set -f
+array=(${DOMAINS//,/ })
+for i in "${!array[@]}"
+do
+    echo "$i=>${array[i]} and www.${array[i]}"
+    TLD="${array[i]}"
+    domain="${TLD%\"}"
+    domain="${domain#\"}"
+
+    if [[ "$environment" == "production" ]]; then
+        #for production    
+        letsencrypt certonly --webroot -w /var/www/letsencrypt -d $domain -d "www."$domain  --agree-tos --email $CONTACT --non-interactive --text
+
+        echo "copying certificates to server..."
+        
+        #copy live certs if any
+        mkdir -p "/var/certs/$domain" && cp "/etc/letsencrypt/live/$domain/cert.pem"      $_
+        mkdir -p "/var/certs/$domain" && cp "/etc/letsencrypt/live/$domain/chain.pem"     $_
+        mkdir -p "/var/certs/$domain" && cp "/etc/letsencrypt/live/$domain/fullchain.pem" $_
+        mkdir -p "/var/certs/$domain" && cp "/etc/letsencrypt/live/$domain/privkey.pem"   $_
+
+    else
+        #for testing
+        letsencrypt certonly --webroot -w /var/www/letsencrypt -d $domain -d "www."$domain  --agree-tos --email $CONTACT --non-interactive --text --dry-run
+    fi
+done
+#-----------------------------------------------------------------------
+echo "certbot execution at 100%...."
+#-----------------------------------------------------------------------
 
