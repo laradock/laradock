67519a2a2f92e53082532d5a2f968495b4981929
erikn69 <erikn_69@hotmail.com>
2024-11-26 11:06:44 -0500

    Fix `edit this page` links

--- DOCUMENTATION/docusaurus.config.ts
+++ DOCUMENTATION/docusaurus.config.ts
@@ -39,7 +39,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/laradock/laradock/edit/master/DOCUMENTATION/',
         },
         // blog: {
         //   showReadingTime: true,
